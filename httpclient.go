2b75b536d2355903ee5aa7e2548e13dd7c20b594
Mark Salpeter <marksalpeter@gmail.com>
2023-08-26 22:39:22 +0200

    fix: nil edge case in http response

--- httpclient.go
+++ httpclient.go
@@ -7,17 +7,18 @@ import (
 
 // HTTPClient is a wrapper around http.Client that implements retry logic
 type httpClient struct {
-	client *http.Client
+	client     *http.Client
 	MaxBackOff time.Duration
 	MinBackOff time.Duration
-	Retries int
+	Retries    int
 }
+
 // Do executes the http request with retry logic
 func (c *httpClient) Do(req *http.Request) (*http.Response, error) {
 	// Execute the request
 	var resp *http.Response
 	var err error
-	for i := 1; i <= c.Retries + 1; i++ {
+	for i := 1; i <= c.Retries+1; i++ {
 		// Execute the request
 		resp, err = c.client.Do(req)
 		// If there is an error or the status code is not in the 200's, wait and try again
@@ -26,10 +27,9 @@ func (c *httpClient) Do(req *http.Request) (*http.Response, error) {
 			continue
 		}
 		// Return the successful response
-		return resp, nil
+		break
 	}
-	// Return the error
-	return nil, err
+	return resp, err
 }
 
 // isStatusOK returns true if the status code is between 200 and 299
@@ -37,7 +37,7 @@ func (c *httpClient) isStatusOK(statusCode int) bool {
 	return statusCode >= 200 && statusCode < 300
 }
 
-// getExponentialBackOffDuration returns a the exponential back off duration between 
+// getExponentialBackOffDuration returns a the exponential back off duration between
 // the min and max values based on the number of attempted requests
 func (c *httpClient) getExponentialBackOffDuration(attempt int) time.Duration {
 	exp := c.MinBackOff
